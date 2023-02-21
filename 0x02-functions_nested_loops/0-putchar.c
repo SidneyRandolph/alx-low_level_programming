364f15a8163e86187cda5b2ad073ac83fd3cec26
SidneyRandolph <randolphsidney8@gmail.com>
2023-02-21 07:18:15 -0800

    putchar spelling

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
  * main - prints _putchar and a new line
- *
- * Return: Always 0 in success
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar(95); /*95 is asci for _*/
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
